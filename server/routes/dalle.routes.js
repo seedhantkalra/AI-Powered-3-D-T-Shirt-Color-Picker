3ca7e3b4ab5512f54543b48724e5ca8d7d0e48ed
Bharat Kalra (DIGITAL INTELLIGENCE SYSTEMS L) <v-bkalra@microsoft.com>
2024-06-09 21:59:52 -0400

    first commit

---
+++ server/routes/dalle.routes.js
@@ -0,0 +1,40 @@
+import express, { response } from 'express';
+import * as dotenv from 'dotenv';
+import OpenAI from 'openai';
+
+dotenv.config();
+
+const router = express.Router();
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY // This is also the default, can be omitted
+});
+
+
+router.route('/').get((req, res) => {
+    res.status(200).json({ message: "Hello from DALL.E 2.0" })
+})
+
+router.route('/').post(async (req, res) => {
+    try {
+      const { prompt } = req.body;  
+      
+      const response = await openai.createImage({
+        prompt,
+        n: 1,
+        size: '1024x1024',
+        response_format: 'b64_json'
+      });
+
+      const image = response.data.data[0].b64_json;
+
+      res.status(200).json({ photo: image });
+
+    } catch (error) {
+      console.error("Detailed Error: ", error);
+      res.status(500).json({ message: "Something Went Wrong" })
+    }
+})
+
+export default router;
+
